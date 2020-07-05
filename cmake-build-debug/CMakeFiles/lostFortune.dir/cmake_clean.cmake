55c6fafb479143d79eb106ef48e4843bb8c6b11a
Jimin Huh <jiminhuh95@gmail.com>
2020-07-04 21:50:55 -0500

    initialize git

---
+++ cmake-build-debug/CMakeFiles/lostFortune.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lostFortune.dir/main.cpp.o"
+  "lostFortune"
+  "lostFortune.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lostFortune.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/lostFortune.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for lostFortune.
+# This may be replaced when dependencies are built.
